aa5564a403074dfaa34a27240681b86199a6547c
arjun-sudo <arjungautam5431@gmail.com>
2020-12-18 12:50:36 +0545

    Post and get completed

---
+++ SparkCRUD/src/main/java/controller/SparkREST.java
@@ -0,0 +1,50 @@
+/**
+ * @author :arjun
+ * Project :SparkCRUD
+ * Date : 2020-12-18
+ * Time : 12:00
+ */
+package controller;
+
+import com.google.gson.Gson;
+import model.User;
+import service.UserService;
+import service.UserServiceMapImpl;
+import status.StandardResponse;
+import status.StatusResponse;
+
+import static spark.Spark.*;
+
+public class SparkREST {
+    public static void main(String[] args) {
+
+        final UserService userService = new UserServiceMapImpl();
+
+
+        /*Add User*/
+        post("/users", (request, response) -> {
+            response.type("application/json");
+
+            User user = new Gson().fromJson(request.body(), User.class);
+
+            userService.addUser(user);
+            return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS));
+        });
+
+        /*Get Users*/
+        get("/users", (request, response) -> {
+            response.type("application/json");
+
+            return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS, new Gson().toJsonTree(userService.getUsers())));
+        });
+
+        /*Get User*/
+        get("/users/:id", (request, response) -> {
+            response.type("application/json");
+
+            return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS, new Gson().toJsonTree(userService.getUser(request.params(":id")))));
+
+        });
+
+    }
+}
---
+++ SparkCRUD/src/main/java/service/UserServiceMapImpl.java
@@ -0,0 +1,72 @@
+/**
+ * @author :arjun
+ * Project :SparkCRUD
+ * Date : 2020-12-18
+ * Time : 12:05
+ */
+package service;
+
+import exception.UserException;
+import model.User;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+public class UserServiceMapImpl implements UserService {
+    private HashMap<String, User> userHashMap;
+
+    public UserServiceMapImpl() {
+        userHashMap = new HashMap<>();
+    }
+
+    @Override
+    public void addUser(User user) {
+        userHashMap.put(user.getId(), user);
+    }
+
+    @Override
+    public Collection<User> getUsers() {
+        return userHashMap.values();
+    }
+
+    @Override
+    public User getUser(String id) {
+        return userHashMap.get(id);
+    }
+
+    @Override
+    public User editUser(User forEdit) throws UserException {
+        try {
+            if (forEdit.getId() == null)
+                throw new UserException("ID cannot be blank ");
+            User toEdit = userHashMap.get(forEdit.getId());
+
+            if (toEdit == null)
+                throw new UserException("User not Found");
+
+            if (forEdit.getEmail() != null) {
+                toEdit.setEmail(forEdit.getEmail());
+            }
+            if (forEdit.getFirstName() != null) {
+                toEdit.setFirstName(forEdit.getFirstName());
+            }
+            if (forEdit.getId() != null) {
+                toEdit.setId(forEdit.getId());
+            }
+            return toEdit;
+
+        } catch (Exception e) {
+            throw new UserException(e.getMessage());
+        }
+    }
+
+    @Override
+    public void deleteUser(String id) {
+        userHashMap.remove(id);
+    }
+
+    @Override
+    public boolean userExist(String id) {
+        return userHashMap.containsKey(id);
+    }
+}
