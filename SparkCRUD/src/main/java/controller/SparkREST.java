66cc80a68ce2a1b9fedd955ada68f08b05072d80
arjun-sudo <arjungautam5431@gmail.com>
2020-12-18 13:03:20 +0545

    Delete and Options

--- SparkCRUD/src/main/java/controller/SparkREST.java
+++ SparkCRUD/src/main/java/controller/SparkREST.java
@@ -45,21 +45,30 @@ public class SparkREST {
             return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS, new Gson().toJsonTree(userService.getUser(request.params(":id")))));
         });
 
-        put("/users/:id",((request, response) -> {
+        put("/users/:id", ((request, response) -> {
             response.type("application/json");
 
-            User toEdit=new Gson().fromJson(request.body(),User.class);
-            User editedUser=userService.editUser(toEdit);
+            User toEdit = new Gson().fromJson(request.body(), User.class);
+            User editedUser = userService.editUser(toEdit);
 
-            if(editedUser!=null){
-                return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS,new Gson().toJsonTree(editedUser)));
-            }
-            else {
-                return new Gson().toJson(new StandardResponse(StatusResponse.ERROR,new Gson().toJsonTree("User not found or error in edit")));
+            if (editedUser != null) {
+                return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS, new Gson().toJsonTree(editedUser)));
+            } else {
+                return new Gson().toJson(new StandardResponse(StatusResponse.ERROR, new Gson().toJsonTree("User not found or error in edit")));
             }
         }));
 
+        delete("/users/:id", (request, response) -> {
+            response.type("application/json");
 
+            userService.deleteUser(request.params(":id"));
+            return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS, "User deleted"));
+        });
 
+        options("/users/:id", (request, response) -> {
+            response.type("application/json");
+
+            return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS, (userService.userExist(request.params(":id"))) ? "User Exists" : "User does not exists"));
+        });
     }
 }
